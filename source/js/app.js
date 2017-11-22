47abe1177919455d436632e4423c4769e3d1d14b
Alexander <dev@salex.pro>
2017-11-22 19:22:27 +0200

    slide2

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,24 @@
-var slide = 1;
-var wrapper = document.querySelector('.tutorial');
-var header = document.querySelector('.tutorial_header')
+var slide = 1,
+    wrapper = document.querySelector('.tutorial'),
+    wrapper_class = wrapper.classList,
+    header  = document.querySelector('.tutorial_header'),
+    header_class = header.classList,
+    image   = document.querySelector('.tutorial_img img');
+
 wrapper.addEventListener('click', function () {
+    change_slide();
+});
+
+function change_slide() {
     slide++;
+    wrapper_class.add('slide--' + slide);
     switch (slide) {
         case 2:
-            header.innerHTML = '';
+            header.innerHTML = 'Чтобы выбрать материал для обучения нажмите в «Меню» на кнопку «Обучение».<br>После выберете тренинг, который хотите изучить.';
+            header_class.remove('tutorial_header--center');
+            header_class.add('tutorial_header--left');
             break;
         case 3:
             break;
     }
-});
+}
