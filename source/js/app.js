14b3a5ad5aa0c2290355d9980b51d6f95d12035f
Alexander <dev@salex.pro>
2017-11-23 22:35:17 +0200

    slide4

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,8 @@
+// Tutorial
 var slide = 1,
     wrapper = document.querySelector('.tutorial'),
     wrapper_class = wrapper.classList,
-    header  = document.querySelector('.tutorial_header'),
-    header_class = header.classList,
-    image   = document.querySelector('.tutorial_img img');
+    header  = document.querySelector('.tutorial_header');
 
 wrapper.addEventListener('click', function () {
     change_slide();
@@ -15,12 +14,12 @@ function change_slide() {
     switch (slide) {
         case 2:
             header.innerHTML = 'Чтобы выбрать материал для обучения нажмите в «Меню» на кнопку «Обучение».<br>После выберете тренинг, который хотите изучить.';
-            header_class.remove('tutorial_header--center');
-            header_class.add('tutorial_header--left');
             break;
         case 3:
-            image.src = './img/3.png';
             header.innerHTML = 'Чтобы начать обучение,<br>нажмите на кнопку «Начать<br>обучение».<div class="button">НАЧАТЬ ОБУЧЕНИЕ</div>';
+            break;
+        case 4:
+            header.innerHTML = 'Нажмите на язычок «Меню»,<br> чтобы открыть Меню для<br> перехода в другие разделы сайта<div class="tutorial_menu">Меню</div>';
             break;
     }
 }
