1a0cf07cc68aa39a916e6a9faaf1934174cbb071
Alexander <dev@salex.pro>
2017-11-25 21:39:34 +0200

    autoswitch

--- README.md
+++
@@ -1,1 +0,0 @@
-Acuvue Tutorial
--- source/js/app.js
+++ source/js/app.js
@@ -8,9 +8,18 @@ wrapper.addEventListener('click', function () {
     change_slide();
 });
 
+var change = setInterval(function() {
+    change_slide();
+}, 8000);
+
 function change_slide() {
-    slide++;
-    wrapper_class.add('slide--' + slide);
+    if(slide < 11){
+        slide++;
+        wrapper_class.add('slide--' + slide);
+    } else {
+        clearInterval(change);
+        return false;
+    }
     switch (slide) {
         case 2:
             header.innerHTML = 'Чтобы выбрать материал для обучения нажмите в «Меню» на кнопку «Обучение».<br>После выберете тренинг, который хотите изучить.';
@@ -30,8 +39,11 @@ function change_slide() {
         case 7:
             header.innerHTML = 'Нажмите «Продолжить» после окончания видео ролика, чтобы перейти к следующему разделу<div class="button">Продолжить</div>';
             break;
+        case 8:
+            header.innerHTML = 'Введите любой логин и пароль.<br>Например, Логин – ABCDEF, Пароль – 123456. Нажмите на кнопку – ВОЙТИ. И потренируйтесь в работе с приложением для сотрудников оптик.<div class="button button--blue">войти</div>';
+            break;
         case 9:
-            header.innerHTML = 'Введите любой логин и пароль.<br>Например, Логин – ABCDEF, Пароль – 123456<div class="button button--blue">войти</div>';
+            header.innerHTML = 'Нажмите «Продолжить» после окончания видео ролика, чтобы перейти к следующему разделу<div class="button">Продолжить</div>';
             break;
         case 10:
             header.innerHTML = 'Нажмите «Начать тест», чтобы<br>пройти тестирование своих<br>знаний по обучающему<br>материалу<div class="button">Начать тест</div>';
@@ -39,5 +51,44 @@ function change_slide() {
         case 11:
             header.innerHTML = 'Для того, чтобы выбрать<br>вариант ответа нажмите на<br>него После нажатия<br> изменится цвет на темно<br>синий';
             break;
+        default:
+            break;
     }
 }
+
+// Resize
+var $el = document.querySelector('.tutorial_inner');
+var elWidth = $el.clientWidth;
+var elHeight = $el.clientHeight;
+
+var $wrapper = wrapper;
+
+window.onresize = function() {
+    var sizes = {
+        size: {
+            width: $wrapper.clientWidth,
+            height: $wrapper.clientHeight
+        }
+    }
+    doResize(null, sizes)
+}
+
+function doResize(event, ui) {
+
+    var scale, origin;
+
+    scale = Math.min(
+        ui.size.width / elWidth,
+        ui.size.height / elHeight
+    );
+
+    $el.style = 'transform: translate3d(-50%, -50%, 0) scale(' + scale + ')';
+}
+
+var starterData = {
+    size: {
+        width: $wrapper.clientWidth,
+        height: $wrapper.clientHeight
+    }
+}
+doResize(null, starterData);
